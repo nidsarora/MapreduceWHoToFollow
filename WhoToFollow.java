72a2dccf85e85b83c768b15c1a7dc51562c4debe
Nidhi Arora <aroranidhi10@gmail.com>
2017-02-04 13:34:00 -0500

    Final uncommented WhoToFollow
    Final uncommented WhoToFollow with input.txt and outputs :outputTemp.part-r-000000(Output of first MapReduce) and outputFinal.part-r-000000(output of second MapReduce)

--- WhoToFollow.java
+++ WhoToFollow.java
@@ -1,6 +1,7 @@
   import java.io.IOException;
   import java.util.StringTokenizer;
   import java.util.*;
+  import java.util.function.Predicate;
   import org.apache.hadoop.conf.Configuration;
   import org.apache.hadoop.fs.Path;
   import org.apache.hadoop.io.IntWritable;
@@ -11,8 +12,6 @@
   import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
   import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  
-
-
 
   public class WhoToFollow {
 
@@ -63,20 +62,160 @@
         }
 }
 
+public static class AllPairsMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+
+        public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(values.toString());
+            IntWritable user = new IntWritable(Integer.parseInt(st.nextToken()));
+            ArrayList<Integer> friends = new ArrayList<>();
+            IntWritable friend1 = new IntWritable();
+            while (st.hasMoreTokens()) {
+               
+                Integer friend = Integer.parseInt(st.nextToken());
+                if(friend>0)
+                friends.add(friend); 
+                else
+                {IntWritable existing_friend=new IntWritable();
+                existing_friend.set(friend);
+                context.write(user,existing_friend);
+                }
+
+            }
+         
+            ArrayList<Integer> seenFriends = new ArrayList<>();
+            IntWritable friend2 = new IntWritable();
+            for (Integer friend : friends) {
+                friend1.set(friend);
+                for (Integer seenFriend : seenFriends) {
+                    friend2.set(seenFriend);
+                    context.write(friend1, friend2);
+                    context.write(friend2, friend1);
+                }
+                seenFriends.add(friend1.get());
+            }
+        }
+    }
+
+    public static class RecommenderReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+
+        private static class Recommendation {
+
+           
+            private int friendId;
+            private int nCommonFriends;
+
+           
+            public Recommendation(int friendId) {
+                this.friendId = friendId;
+                this.nCommonFriends = 1;
+            }
+
+        
+            public int getFriendId() {
+                return friendId;
+            }
+
+            public int getNCommonFriends() {
+                return nCommonFriends;
+            }
+
+          
+            public void addCommonFriend() {
+                nCommonFriends++;
+            }
+
+              
+            public String toString() {
+                return friendId + "(" + nCommonFriends + ")";
+            }
+            public static Recommendation find(int friendId, ArrayList<Recommendation> recommendations) {
+                for (Recommendation p : recommendations) {
+                    if (p.getFriendId() == friendId) {
+                        return p;
+                    }
+                }
+                return null;
+            }
+        }
+
+           
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            IntWritable user = key;
+            ArrayList<Integer> existingFriends = new ArrayList();
+            ArrayList<Integer> recommendedUsers = new ArrayList<>();
+            while (values.iterator().hasNext()) {
+                int value = values.iterator().next().get();
+                if (value > 0) {
+                    recommendedUsers.add(value);
+                } else {
+                    existingFriends.add(value);
+                }
+            }
+   
+            for (Integer friend : existingFriends) {
+                recommendedUsers.removeIf(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer t) {
+                        return t.intValue() == -friend.intValue();
+                    }
+                });
+            }
+             ArrayList<Recommendation> recommendations = new ArrayList<>();
+             for (Integer userId : recommendedUsers) {
+                Recommendation p = Recommendation.find(userId, recommendations);
+                if (p == null) {
+                    recommendations.add(new Recommendation(userId));
+                } else {
+                    p.addCommonFriend();
+                }
+            }
+            recommendations.sort(new Comparator<Recommendation>() {
+                @Override
+                public int compare(Recommendation t, Recommendation t1) {
+                    return -Integer.compare(t.getNCommonFriends(), t1.getNCommonFriends());
+                }
+            });
+            StringBuffer sb = new StringBuffer(""); 
+            for (int i = 0; i < recommendations.size() && i < 10; i++) {
+                Recommendation p = recommendations.get(i);
+                sb.append(p.toString() + " ");
+            }
+            Text result = new Text(sb.toString());
+            context.write(user, result);
+        }
+    }
 
 
 
     // Main method
     public static void main(String[] args) throws Exception {
+      
+      String inputDir = "/home/nids/WTFocomb/input.txt";
+      String outputTempDir = "/home/nids/WTFocomb/outputTemp";
+      String outputFinalDir = "/home/nids/WTFocomb/outputFinal";
       Configuration conf = new Configuration();
-      Job job = Job.getInstance(conf, "Who To Follow");
-      job.setJarByClass(WhoToFollow.class);
-      job.setMapperClass(InverterMapper.class);
-      job.setReducerClass(InvertReducer1.class);
-      job.setOutputKeyClass(Text.class);
-      job.setOutputValueClass(Text.class);
-      FileInputFormat.addInputPath(job, new Path(args[0]));
-      FileOutputFormat.setOutputPath(job, new Path(args[1]));
-      System.exit(job.waitForCompletion(true) ? 0 : 1);
+      Job job1 = Job.getInstance(conf, "JOB_1");
+      job1.setJarByClass(WhoToFollow.class);
+      job1.setMapperClass(InverterMapper.class);
+      job1.setReducerClass(InvertReducer1.class);
+      job1.setOutputKeyClass(Text.class);
+      job1.setOutputValueClass(Text.class);
+      FileInputFormat.addInputPath(job1, new Path(inputDir));
+      FileOutputFormat.setOutputPath(job1, new Path(outputTempDir));
+
+      boolean success = job1.waitForCompletion(true);
+      if (success) {
+      Job job2 = Job.getInstance(conf, "JOB_2");
+      job2.setJarByClass(WhoToFollow.class);
+      job2.setJarByClass(WhoToFollow.class);
+      job2.setMapperClass(AllPairsMapper.class);
+      job2.setReducerClass(RecommenderReducer.class);
+      job2.setOutputKeyClass(IntWritable.class);
+      job2.setOutputValueClass(IntWritable.class);
+      FileInputFormat.addInputPath(job2, new Path(outputTempDir));
+      FileOutputFormat.setOutputPath(job2, new Path(outputFinalDir));
+      success = job2.waitForCompletion(true);
+}
+
     }
 }
