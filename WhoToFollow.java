ea3deba94725b1e2dcefaf0d6ecaeacf21ff729e
Nidhi Arora <aroranidhi10@gmail.com>
2017-02-04 09:09:19 -0500

    Add files via upload
    Map and reduce with output as inverted set

--- WhoToFollow.java
+++ WhoToFollow.java
@@ -1,5 +1,6 @@
-  import java.io.IOException;
+import java.io.IOException;
   import java.util.StringTokenizer;
+  import java.util.*;
   import org.apache.hadoop.conf.Configuration;
   import org.apache.hadoop.fs.Path;
   import org.apache.hadoop.io.IntWritable;
@@ -9,23 +10,56 @@
   import org.apache.hadoop.mapreduce.Reducer;
   import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
   import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+ 
+
+
 
   public class WhoToFollow {
 
    
-    public static class InverterMapper extends Mapper<Object, Text, IntWritable, IntWritable>{
+    public static class InverterMapper extends Mapper<Object, Text, Text, Text>{
  
        public void map(Object key, Text value, Context context ) throws IOException, InterruptedException {
           StringTokenizer itr = new StringTokenizer(value.toString());
               if (itr.countTokens() >= 2) {
-                   IntWritable person = new IntWritable(Integer.parseInt(itr.nextToken()));
+                   Text person = new Text();
+                   person.set(itr.nextToken());
                    while (itr.hasMoreTokens()) {
-                      IntWritable follower = new IntWritable(Integer.parseInt(itr.nextToken()));
+                      Text follower = new Text();
+                      follower.set(itr.nextToken());
                       context.write(follower, person);
-                       }
+                      }
                  }
          }
      }
+
+     
+
+ public static class InvertReducer1 extends Reducer<Text, Text, Text, Text>{
+        private Text result = new Text();
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Set<String> persons_to_be_recommended = new HashSet<>();
+            for(Text person: values){
+                persons_to_be_recommended.add(person.toString());
+            }
+            result.set(convertSetToString(persons_to_be_recommended));
+            context.write(key, result);
+        }
+        private static String convertSetToString(Set<String> persons_to_be_recommended){
+            String result = "";
+            Iterator<String> itr = persons_to_be_recommended.iterator();
+            while(itr.hasNext()){
+                result += itr.next();
+                if(itr.hasNext()){
+                    result += " ";
+                }
+            }
+            return result;
+        }
+}
+
+
 
 
     // Main method
@@ -34,13 +68,11 @@
       Job job = Job.getInstance(conf, "Who To Follow");
       job.setJarByClass(WhoToFollow.class);
       job.setMapperClass(InverterMapper.class);
-      job.setOutputKeyClass(IntWritable.class);
-      job.setOutputValueClass(IntWritable.class);
+      job.setReducerClass(InvertReducer1.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(Text.class);
       FileInputFormat.addInputPath(job, new Path(args[0]));
       FileOutputFormat.setOutputPath(job, new Path(args[1]));
       System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
-
-
-
